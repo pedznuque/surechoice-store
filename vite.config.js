f5f67bb9ebf72ee45047c0a34170d5f89f185c3d
Gerald Nuque <you@example.com>
2025-02-07 14:46:08 +0800

    initial Upload

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-
-    host: '192.168.0.101',
-    port: 3001
-
-  }
+  base: "/surechoice-store/"
 
 })

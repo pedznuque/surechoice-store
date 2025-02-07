be786c99401d43bd264eae2cd5d7c2de8bf49db8
Gerald Nuque <you@example.com>
2025-02-07 16:49:21 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   base: "/surechoice-store/",
   plugins: [react()]
   
-=======
-  plugins: [react()],
- base: "/"
->>>>>>> af32b2e7cc41752cb7ee6cf923e5db22c6bfa29c
 
 })
